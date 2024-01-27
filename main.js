7d593d8b610589c9334792eab5a0404a3b0c5c4b
Sajib <p2385553@gmail.com>
2024-01-28 00:04:58 +0600

    Portfolio files html,css, js

---
+++ main.js
@@ -0,0 +1,11 @@
+
+        var typed = new Typed(".text", {
+          strings: ["Student" , "Comedian" , "Movies Expert"],
+          typeSpeed: 100,
+          backSpeed: 100,
+          backDelay: 500,
+          loop: true
+          
+        });
+        
+    
